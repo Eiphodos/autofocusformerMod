9eacd77c62355a1c7c8d5d6c8d5646d8fcade42f
Eiphodos <david.hagerman.olzon@gmail.com>
2025-01-10 13:22:19 +0100

    Another attempt at fixing

--- train_net.py
+++ train_net.py
@@ -300,27 +300,28 @@ def main(args):
     cfg = setup(args)
     torch.distributed.barrier()
     import pykeops
-    import tempfile
-    with tempfile.TemporaryDirectory(dir=os.getenv('TMPDIR')) as dirname:
-        pykeops.set_build_folder(dirname)
+    #import tempfile
+    dirname = os.getenv('TMPDIR')
+    #with tempfile.TemporaryDirectory(dir=) as dirname:
+    pykeops.set_build_folder(dirname)
 
-        if args.eval_only:
-            model = Trainer.build_model(cfg)
-            DetectionCheckpointer(model, save_dir=cfg.OUTPUT_DIR).resume_or_load(
-                cfg.MODEL.WEIGHTS, resume=args.resume
-            )
-            res = Trainer.test(cfg, model)
-            if cfg.TEST.AUG.ENABLED:
-                res.update(Trainer.test_with_TTA(cfg, model))
-            if comm.is_main_process():
-                verify_results(cfg, res)
-            return res
+    if args.eval_only:
+        model = Trainer.build_model(cfg)
+        DetectionCheckpointer(model, save_dir=cfg.OUTPUT_DIR).resume_or_load(
+            cfg.MODEL.WEIGHTS, resume=args.resume
+        )
+        res = Trainer.test(cfg, model)
+        if cfg.TEST.AUG.ENABLED:
+            res.update(Trainer.test_with_TTA(cfg, model))
+        if comm.is_main_process():
+            verify_results(cfg, res)
+        return res
 
-        trainer = Trainer(cfg)
-        #trainer.resume_or_load(resume=args.resume)
-        res = trainer.train()
+    trainer = Trainer(cfg)
+    #trainer.resume_or_load(resume=args.resume)
+    res = trainer.train()
 
-        torch.distributed.barrier()
+    torch.distributed.barrier()
         return res
 
 
