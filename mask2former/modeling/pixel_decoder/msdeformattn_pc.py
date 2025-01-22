f76cc0b9b6c1dc241583af1d1dfdce075108f138
Eiphodos <david.hagerman.olzon@gmail.com>
2025-01-22 16:00:07 +0100

    Changed so that aff doesnt use the upsampled decoder

--- mask2former/modeling/pixel_decoder/msdeformattn_pc.py
+++ mask2former/modeling/pixel_decoder/msdeformattn_pc.py
@@ -535,8 +535,5 @@ class MSDeformAttnPixelDecoder(nn.Module):
             if num_cur_levels < self.maskformer_num_feature_levels:
                 multi_scale_features.append(o)
                 num_cur_levels += 1
-        for i, o in enumerate(out):
-            print("Feature map {} from msdeformpoint has shape: {}".format(i, o.shape))
-
 
         return self.mask_features(out[-1]), last_pos, out[0], multi_scale_features, poss[:self.maskformer_num_feature_levels]
