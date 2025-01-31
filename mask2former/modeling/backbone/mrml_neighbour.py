b3031324716b5c2797d93443c2acf48c1f6b4e13
Eiphodos <david.hagerman.olzon@gmail.com>
2025-01-31 14:08:34 +0100

    Contiguous test

--- mask2former/modeling/backbone/mrml_neighbour.py
+++ mask2former/modeling/backbone/mrml_neighbour.py
@@ -691,12 +691,16 @@ class MRMLNB(nn.Module):
                                                          w=min_patched_im_size[1], on_grid=l_idx == 0)
             print("Feature shape after layer {}: {}".format(l_idx, x.shape))
             print("Pos shape after layer {}: {}".format(l_idx, patches_scale_coords.shape))
+            print("Feature is contiguous after layer {}: {}".format(l_idx, x.is_contiguous()))
+            print("Pos is contiguous after layer {}: {}".format(l_idx, patches_scale_coords.is_contiguous()))
             #outs["res{}_spatial_shape".format(out_idx)] = patched_im_size
             if l_idx < self.n_scales - 1:
                 x, patches_scale_coords, meta_loss, meta_loss_coord = self.split_input(x, patches_scale_coords, l_idx,
                                                                                        patched_im_size[0], im)
                 print("Feature shape after split in layer {}: {}".format(l_idx, x.shape))
                 print("Pos shape after split in layer {}: {}".format(l_idx, patches_scale_coords.shape))
+                print("Feature is contiguous after split in layer {}: {}".format(l_idx, x.is_contiguous()))
+                print("Pos is contiguous after split in layer {}: {}".format(l_idx, patches_scale_coords.is_contiguous()))
                 PS /= 2
                 patched_im_size = (H // PS, W // PS)
                 x = self.downsamplers[l_idx](x)
