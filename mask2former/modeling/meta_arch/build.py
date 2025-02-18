fed29baf9d2ddbb3c93d9e944d8e79ef76ad37c6
Eiphodos <david.hagerman.olzon@gmail.com>
2025-02-18 12:36:08 +0100

    MaskPredictor is now SEM_SEG_HEAD instead of meta arch

--- mask2former/modeling/meta_arch/build.py
+++ mask2former/modeling/meta_arch/build.py
@@ -4,8 +4,8 @@ import torch
 from detectron2.utils.logger import _log_api_usage
 from detectron2.utils.registry import Registry
 
-META_ARCH_REGISTRY = Registry("META_ARCH")  # noqa F401 isort:skip
-META_ARCH_REGISTRY.__doc__ = """
+SEM_SEG_HEADS_REGISTRY = Registry("SEM_SEG_HEADS")  # noqa F401 isort:skip
+SEM_SEG_HEADS_REGISTRY.__doc__ = """
 Registry for meta-architectures, i.e. the whole model.
 
 The registered object will be called with `obj(cfg)`
@@ -21,7 +21,7 @@ def build_mask_predictor_indexed(cfg, layer_index):
         an instance of :class:`Backbone`
     """
 
-    mask_predictor = cfg.MODEL.MASK_PREDICTOR
-    model = META_ARCH_REGISTRY.get(mask_predictor)(cfg, layer_index)
+    mask_predictor = cfg.MODEL.SEM_SEG_HEAD.NAME
+    model = SEM_SEG_HEADS_REGISTRY.get(mask_predictor)(cfg, layer_index)
     _log_api_usage("modeling.meta_arch." + mask_predictor)
     return model
