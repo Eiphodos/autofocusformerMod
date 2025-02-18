6fafb260a6c809f333d8731ec9cba9b5b34a1f9b
Eiphodos <david.hagerman.olzon@gmail.com>
2025-02-18 14:45:44 +0100

    Pulled from from config value

--- mask2former/modeling/transformer_decoder/maskfiner_transformer_decoder.py
+++ mask2former/modeling/transformer_decoder/maskfiner_transformer_decoder.py
@@ -25,7 +25,7 @@ def build_transformer_decoder(cfg, layer_index, in_channels, mask_classification
     """
     Build a instance embedding branch from `cfg.MODEL.INS_EMBED_HEAD.NAME`.
     """
-    name = cfg.MODEL.MASK_FORMER.TRANSFORMER_DECODER_NAME
+    name = cfg.MODEL.MASK_FINER.TRANSFORMER_DECODER_NAME
     return TRANSFORMER_DECODER_REGISTRY.get(name)(cfg, layer_index, in_channels, mask_classification)
 
 
