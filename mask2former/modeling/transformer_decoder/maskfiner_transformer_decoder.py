9687e4427180cf73a59754b648b34e56d48b6e43
David <david.hagerman.olzon@gmail.com>
2025-03-01 22:17:09 +0100

    Fixed one bug

--- mask2former/modeling/transformer_decoder/maskfiner_transformer_decoder.py
+++ mask2former/modeling/transformer_decoder/maskfiner_transformer_decoder.py
@@ -482,7 +482,7 @@ class MultiScaleMaskFinerTransformerDecoder(nn.Module):
             predictions_class.append(outputs_class)
             predictions_mask.append(outputs_mask)
 
-        disagreement_mask = self.create_disagreement_mask(pred_mask, outputs_class, mf_pos)
+        disagreement_mask = self.create_disagreement_mask(pred_mask, outputs_class)
 
         assert len(predictions_class) == self.num_layers + 1
         if self.final_layer:
