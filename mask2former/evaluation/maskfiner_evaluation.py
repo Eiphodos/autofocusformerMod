3fe730c34298dd17eda504559e012395c96b11e3
Eiphodos <david.hagerman.olzon@gmail.com>
2025-03-07 09:21:52 +0100

    ok

--- mask2former/evaluation/maskfiner_evaluation.py
+++ mask2former/evaluation/maskfiner_evaluation.py
@@ -23,6 +23,7 @@ class MaskFinerSemSegEvaluator(SemSegEvaluator):
             pred = np.array(output, dtype=int)
 
             gt_filename = self.input_file_to_gt_file[input["file_name"]]
+            print("Ground truth file name: {}".format(gt_filename))
             gt = self.sem_seg_loading_fn(gt_filename, dtype=int)
 
             gt[gt == self._ignore_label] = self._num_classes
