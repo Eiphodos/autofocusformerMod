7a2a379abe12882b3edb19c2eb5ec1a22f62913e
Eiphodos <david.hagerman.olzon@gmail.com>
2025-03-07 12:14:34 +0100

    Position bugfix?

--- mask2former/modeling/transformer_decoder/maskfiner_transformer_decoder.py
+++ mask2former/modeling/transformer_decoder/maskfiner_transformer_decoder.py
@@ -460,17 +460,17 @@ class MultiScaleMaskFinerTransformerDecoder(nn.Module):
         b, _, _ = x[0].shape
         poss_scaled = []
         finest_inp_feat_shape = input_shapes[-1]
-        #print("Mask feature max pos before scaling: {}".format(mf_pos.max()))
+        print("Mask feature max pos before scaling: {}".format(mf_pos.max()))
         mf_pos_scaled = scale_pos(mf_pos, finest_input_shape, finest_inp_feat_shape)
-        #print("Mask feature max pos after scaling: {}".format(mf_pos_scaled.max()))
+        print("Mask feature max pos after scaling: {}".format(mf_pos_scaled.max()))
         i = 0
         for p, inp_shape in zip(pos, input_shapes):
-            #print("Feature {} max pos before scaling: {}".format(i, p.max()))
+            print("Feature {} max pos before scaling: {}".format(i, p.max()))
             pos_scaled = scale_pos(p, finest_input_shape, finest_inp_feat_shape)
-            #print("Feature {} max pos after scaling: {}".format(i, pos_scaled.max()))
+            print("Feature {} max pos after scaling: {}".format(i, pos_scaled.max()))
             poss_scaled.append(pos_scaled)
             i += 1
-        finest_pos = torch.stack(torch.meshgrid(torch.arange(0, finest_inp_feat_shape[0]), torch.arange(0, finest_inp_feat_shape[1]), indexing='ij')).view(2, -1).permute(1, 0)
+        finest_pos = torch.stack(torch.meshgrid(torch.arange(0, finest_inp_feat_shape[0]), torch.arange(0, finest_inp_feat_shape[1]), indexing='ij')).transpose(1,2).reshape(2, -1).permute(1, 0)
         finest_pos = finest_pos.to(mf_pos.device).repeat(b, 1, 1)
 
         for i in range(self.num_feature_levels):
