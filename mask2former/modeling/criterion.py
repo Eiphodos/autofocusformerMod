00687b89727d50a2e201399611f6abdcf463c84b
Eiphodos <david.hagerman.olzon@gmail.com>
2025-03-14 09:10:23 +0100

    Added batch indexz

--- mask2former/modeling/criterion.py
+++ mask2former/modeling/criterion.py
@@ -214,10 +214,10 @@ class SetCriterion(nn.Module):
         outputs_without_aux = {k: v for k, v in outputs.items() if k != "aux_outputs"}
 
         # Retrieve the matching between the outputs of the last layer and the targets
-        print("Output logits main loss shape: {}".format(outputs_without_aux["pred_logits"].shape))
-        print("Output masks main loss shape: {}".format(outputs_without_aux["pred_masks"].shape))
-        print("Target labels main loss shape: {}".format(targets["labels"].shape))
-        print("Target masks main loss shape: {}".format(targets["masks"].shape))
+        print("Output logits main loss shape: {}".format(outputs_without_aux[0]["pred_logits"].shape))
+        print("Output masks main loss shape: {}".format(outputs_without_aux[0]["pred_masks"].shape))
+        print("Target labels main loss shape: {}".format(targets[0]["labels"].shape))
+        print("Target masks main loss shape: {}".format(targets[0]["masks"].shape))
         indices = self.matcher(outputs_without_aux, targets)
 
         # Compute the average number of target boxes accross all nodes, for normalization purposes
@@ -237,8 +237,8 @@ class SetCriterion(nn.Module):
         # In case of auxiliary losses, we repeat this process with the output of each intermediate layer.
         if "aux_outputs" in outputs:
             for i, aux_outputs in enumerate(outputs["aux_outputs"]):
-                print("Output logits aux loss {} shape: {}".format(i, aux_outputs["pred_logits"].shape))
-                print("Output masks aux loss {} shape: {}".format(i, aux_outputs["pred_masks"].shape))
+                print("Output logits aux loss {} shape: {}".format(i, aux_outputs[0]["pred_logits"].shape))
+                print("Output masks aux loss {} shape: {}".format(i, aux_outputs[0]["pred_masks"].shape))
                 indices = self.matcher(aux_outputs, targets)
                 for loss in self.losses:
                     l_dict = self.get_loss(loss, aux_outputs, targets, indices, num_masks)
