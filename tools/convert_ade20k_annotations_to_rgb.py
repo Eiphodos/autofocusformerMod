d2429e49ad9748ac1b261dceebce702586b09bcd
Eiphodos <david.hagerman.olzon@gmail.com>
2025-03-26 10:39:51 +0100

    Convert annotations to more visible and consistent with output from network.

---
+++ tools/convert_ade20k_annotations_to_rgb.py
@@ -0,0 +1,35 @@
+from PIL import Image
+import numpy as np
+import os
+from pathlib import Path
+import tqdm
+import random
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+
+
+
+def convert_to_rgb(input, output):
+    img = np.asarray(Image.open(input))
+    hsv_colors = [(i / 150, 0.75, 0.75) for i in range(150)]
+    random.Random(1337).shuffle(hsv_colors)
+    rgb_colors = [mcolors.hsv_to_rgb(hsv) for hsv in hsv_colors]
+    color_map = (np.array(rgb_colors) * 255).astype(np.uint8)
+    H, W = img.shape
+    rgb_image = np.zeros((H, W, 3), dtype=np.uint8)
+    for label in range(150):
+        rgb_image[img == label] = color_map[label]
+    image = Image.fromarray(rgb_image)
+
+    image.save(output)
+
+
+if __name__ == "__main__":
+    dataset_dir = Path(os.getenv("DETECTRON2_DATASETS", "datasets")) / "ADEChallengeData2016"
+    for name in ["training", "validation"]:
+        annotation_dir = dataset_dir / "annotations" / name
+        output_dir = dataset_dir / "annotations_detectron2_rgb" / name
+        output_dir.mkdir(parents=True, exist_ok=True)
+        for file in tqdm.tqdm(list(annotation_dir.iterdir())):
+            output_file = output_dir / file.name
+            convert_to_rgb(file, output_file)
