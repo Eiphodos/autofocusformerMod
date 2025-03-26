8cb01cc19393083916c7d1a0d942b270eab6a54a
Eiphodos <david.hagerman.olzon@gmail.com>
2025-03-26 10:44:52 +0100

    Dont need to create the color map for every output

--- tools/convert_ade20k_annotations_to_rgb.py
+++ tools/convert_ade20k_annotations_to_rgb.py
@@ -7,14 +7,15 @@ import random
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 
-
-
-def convert_to_rgb(input, output):
-    img = np.asarray(Image.open(input))
+def create_colormap():
     hsv_colors = [(i / 150, 0.75, 0.75) for i in range(150)]
     random.Random(1337).shuffle(hsv_colors)
     rgb_colors = [mcolors.hsv_to_rgb(hsv) for hsv in hsv_colors]
     color_map = (np.array(rgb_colors) * 255).astype(np.uint8)
+
+    return color_map
+def convert_to_rgb(input, output, color_map):
+    img = np.asarray(Image.open(input))
     H, W = img.shape
     rgb_image = np.zeros((H, W, 3), dtype=np.uint8)
     for label in range(150):
@@ -30,6 +31,9 @@ if __name__ == "__main__":
         annotation_dir = dataset_dir / "annotations" / name
         output_dir = dataset_dir / "annotations_detectron2_rgb" / name
         output_dir.mkdir(parents=True, exist_ok=True)
+
+        color_map = create_colormap()
+
         for file in tqdm.tqdm(list(annotation_dir.iterdir())):
             output_file = output_dir / file.name
-            convert_to_rgb(file, output_file)
+            convert_to_rgb(file, output_file, color_map)
