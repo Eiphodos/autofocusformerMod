916e54d76a97adfebf5815bb89f899a34d2579c1
Eiphodos <david.hagerman.olzon@gmail.com>
2025-03-26 09:13:46 +0100

    wrong argument (self)

--- mask2former/evaluation/maskfiner_evaluation.py
+++ mask2former/evaluation/maskfiner_evaluation.py
@@ -26,7 +26,7 @@ class MaskFinerSemSegEvaluator(SemSegEvaluator):
             gt_filename = self.input_file_to_gt_file[input["file_name"]]
             gt = self.sem_seg_loading_fn(gt_filename, dtype=int)
 
-            self.save_error_map(self, pred, gt, input["file_name"])
+            self.save_error_map(pred, gt, input["file_name"])
 
             gt[gt == self._ignore_label] = self._num_classes
 
