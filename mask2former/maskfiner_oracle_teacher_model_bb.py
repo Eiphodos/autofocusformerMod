d67871a401c90d074825317819046a2cc3059f4d
Eiphodos <david.hagerman.olzon@gmail.com>
2025-08-14 23:08:43 +0200

    revert

--- mask2former/maskfiner_oracle_teacher_model_bb.py
+++ mask2former/maskfiner_oracle_teacher_model_bb.py
@@ -287,7 +287,8 @@ class MaskFinerOracleTeacherBB(nn.Module):
             mask_pred_results = F.interpolate(
                 mask_pred_results,
                 size=(images.tensor.shape[-2], images.tensor.shape[-1]),
-                mode="nearest-exact"
+                mode="bilinear",
+                align_corners=False,
             )
 
             del outputs
